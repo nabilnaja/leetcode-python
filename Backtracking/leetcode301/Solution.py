0b46195e48d851005abadac36c644044ad6a813b
nabilnaja <nabilnaja9@gmail.com>
2022-04-19 22:00:53 +0200

    leetcode 301 add solution wip

---
+++ Backtracking/leetcode301/Solution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        pass
+
+if __name__ == '__main__':
+    print(Solution().removeInvalidParentheses('()())()'))
