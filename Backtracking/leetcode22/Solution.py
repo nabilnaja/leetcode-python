867bf82eb4654e49d5521b9f277c87e4f0cc6ca9
nabilnaja <nabilnaja9@gmail.com>
2022-04-21 19:11:31 +0200

    leetcode 22 add solution

--- Backtracking/leetcode22/Solution.py
+++ Backtracking/leetcode22/Solution.py
@@ -6,18 +6,16 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         def backtrack(sol, s=[], leftSum=0, rightSum=0):
             if len(s) == n * 2:
-                if leftSum == rightSum and len(s) == n * 2:
-                    sol.add(''.join(s))
+                sol.append(''.join(s))
                 return
-
             if leftSum > rightSum:
                 s.append(')')
                 backtrack(sol, s, leftSum, rightSum + 1)
                 s.pop()
-
-            s.append('(')
-            backtrack(sol, s, leftSum + 1, rightSum)
-            s.pop()
+            if leftSum < n:
+                s.append('(')
+                backtrack(sol, s, leftSum + 1, rightSum)
+                s.pop()
 
         result = []
         backtrack(result)
