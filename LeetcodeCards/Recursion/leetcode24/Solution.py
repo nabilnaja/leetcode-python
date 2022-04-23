eaf545c13cdf70cdbdf089b508e201fddfd64473
nabilnaja <nabilnaja9@gmail.com>
2022-04-23 16:31:51 +0200

    leetcode 24 add solution

---
+++ LeetcodeCards/Recursion/leetcode24/Solution.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from DataStructure.datastructure import ListNode
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def swap(node):
+            if not node or not node.next:
+                return node
+            first = node
+            second = first.next
+            first.next = swap(second.next)
+            second.next = first
+            return second
+
+        return swap(head)
