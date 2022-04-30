e1dbaa1139ef3a520b66080c77da643ad47725f7
nabilnaja <nabilnaja9@gmail.com>
2022-04-30 23:10:51 +0200

    leetcode 1770 add solution

--- LeetcodeCards/DynamicProgramming/leetcode1770/Solution.py
+++ LeetcodeCards/DynamicProgramming/leetcode1770/Solution.py
@@ -5,18 +5,15 @@ from typing import List
 class Solution:
 
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
-
         @lru_cache(2000)
-        def dp(i, left):
-            # Base case
+        def dp(i, l):
             if i == m:
                 return 0
+            j = i + 1
+            left = dp(j, l + 1) + multipliers[i] * nums[l]
+            right = dp(j, l) + multipliers[i] * nums[n - (i - l) - 1]
 
-            mult = multipliers[i]
-            right = n - 1 - (i - left)
-
-            return max(mult * nums[left] + dp(i + 1, left + 1),
-                       mult * nums[right] + dp(i + 1, left))
+            return max(left, right)
 
         n, m = len(nums), len(multipliers)
         return dp(0, 0)
